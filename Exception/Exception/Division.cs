dc250a1eaac61977b787adb78e9ca19c37b9853e
Manoj naik <chittinaik@gamil.com>
2023-09-22 09:29:11 +0530

    [Manoj] add:Division By Zero

---
+++ Exception/Exception/Division.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exception
+{
+    internal class Division
+    {
+        public void Num()
+        {
+            int a = 0;
+            //if (a != 0)
+            //{
+            //    int b = 10 / a;
+            //    Console.WriteLine(b);
+            //}
+            try
+            {
+                int b = 10 / a;
+            }
+            catch (DivideByZeroException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+           
+        }
+    }
+}
---
+++ Exception/Exception/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exception
+{
+    internal class Program
+    {
+        //A problem that occurs during the  excution of a program is refer to as an excution
+        //It allow to control a passed from one part of the programe to the another exception
+        //It will prevent the crashing of the programe
+        static void Main(string[] args)
+        {
+            Division d=new Division();
+            d.Num();
+            Console.ReadLine();
+        }
+    }
+}
